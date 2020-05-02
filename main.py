f484c7ded67620d39ceda22414f0508ff7f51549
rcv911 <echinopsine@gmail.com>
2020-05-02 15:38:19 +0300

    ver 0.1.0

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
 from app import app
 import view
 import models
 
 if __name__ == '__main__':
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    storage = f'{basedir}/photo_storage'
+    if not os.path.exists(storage):
+        os.mkdir(storage)
     app.run()
